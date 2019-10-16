32549d60ee378983fa0d2f816dd7dcae0831899a
qiucy <zhuaianq@gmail.com>
2019-10-16 17:54:13 +0800

    T101_对称二叉树

--- src/main/java/cn/zhueian/algorithm/搜索/T101_对称二叉树.java
+++ src/main/java/cn/zhueian/algorithm/搜索/T101_对称二叉树.java
@@ -1,5 +1,10 @@
 package cn.zhueian.algorithm.搜索;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * Created by qiucy on 2019/10/14.
  * 给定一个二叉树，检查它是否是镜像对称的。
@@ -22,8 +27,32 @@ package cn.zhueian.algorithm.搜索;
  */
 public class T101_对称二叉树 {
     public boolean isSymmetric(TreeNode root) {
+        boolean res = true;
+        if (root == null ) return res=true;
+        Queue<TreeNode> var1 = new LinkedList<>();
+        List<String> comPlace = new ArrayList<>();
+        var1.add(root);
+        for (;!var1.isEmpty();){
+            comPlace.clear();
+            int size = var1.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = var1.poll();
+                TreeNode left = poll.left;
+                TreeNode right = poll.right;
+                comPlace.add(left == null?"null":left.val+"");
+                comPlace.add(right == null?"null":right.val+"");
+                if (left!=null) var1.add(left);
+                if (right!=null) var1.add(right);
+            }
+            for (int j = 0;j<comPlace.size()/2;j++)
+                if (!comPlace.get(j).equals(comPlace.get(comPlace.size()-j-1))) return false;
+        }
+        return res;
+    }
 
-        return false;
+    public static void main(String[] args) {
+        T101_对称二叉树 t = new T101_对称二叉树();
+        System.out.println(t.isSymmetric(null));
     }
 }
 class TreeNode {
